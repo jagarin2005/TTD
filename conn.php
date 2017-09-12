3ac3b51415ad25ea761b90856f543a586a6a6acb
ITALEI <jagarin2005@gmail.com>
2017-09-13 00:20:00 +0700

    Deploy! [bug]

---
+++ conn.php
@@ -0,0 +1,26 @@
+<?php 
+
+session_start();
+date_default_timezone_set('Asia/Bangkok');
+
+$server = "";
+$db = "ttd";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$server;dbname=$db", $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // echo "Connection successfully";
+} catch(PDOException $e) {
+  // echo "Connection failed : ". $e->getMessage();
+}
+require_once 'Mailer.php';
+require_once 'User.php';
+$user = new User($conn);
+
+// ลบกะวันที่ผ่านมาแล้ว 1 สัปดาห์
+// $adShifts = $conn->prepare("DELETE FROM shifts WHERE s_date < :ago");
+// $adShifts->execute(array(":ago"=>date('Y-m-d', strtotime('-1 week'))));
+
+?>
